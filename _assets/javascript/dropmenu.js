547456188371c8095d463521ed2599b0ae94def9
Grant Blakeman <hello@grantblakeman.com>
2018-11-29 14:06:07 -0800

    js - consolidate into single load

--- _assets/javascript/dropmenu.js
+++ _assets/javascript/dropmenu.js
@@ -76,14 +76,3 @@
 }());
 
 ready(DropMenu.init);
-
-// ---------------------------------------------- first-load boilerplate stuff below
-
-// fire once DOM is loaded
-function ready(fn) {
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', fn);
-  } else {
-    fn();
-  }
-}
